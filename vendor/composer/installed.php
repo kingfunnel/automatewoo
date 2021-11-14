75298df1fe378b9703bb24e52c8d4c86db2247ab
anpimartinez <kingfunnel.co@gmail.com>
2021-11-14 17:49:34 -0500

    AutomateWoo 5.5.5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-release/5.5.5',
+        'version' => 'dev-release/5.5.5',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6058cd6d3bd7b319d53da6ce5d453561a56e9588',
+        'name' => 'woocommerce/automatewoo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'woocommerce/automatewoo' => array(
+            'pretty_version' => 'dev-release/5.5.5',
+            'version' => 'dev-release/5.5.5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6058cd6d3bd7b319d53da6ce5d453561a56e9588',
+            'dev_requirement' => false,
+        ),
+    ),
+);
